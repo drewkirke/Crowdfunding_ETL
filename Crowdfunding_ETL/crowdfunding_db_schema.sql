cb69e1ef2db6106c78b0e2f3928c41b2d499b620
drewkirke <152921668+drewkirke@users.noreply.github.com>
2024-03-25 21:16:44 -0700

    Update crowdfunding_db_schema.sql

--- Crowdfunding_ETL/crowdfunding_db_schema.sql
+++ Crowdfunding_ETL/crowdfunding_db_schema.sql
@@ -1,1 +1,4 @@
 select * from crowdfunding_db_schema.campaign
+select * from crowdfunding_db_schema.contacts
+select * from crowdfunding_db_schema.category
+select * from crowdfunding_db_schema.subcategory
